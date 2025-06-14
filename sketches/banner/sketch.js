92869e50122240dcb028c2967e3d41a21e1a23fd
Cosine <cosine_wave@icloud.com>
2025-06-14 12:20:22 +0200

    updated banner

--- sketches/banner/sketch.js
+++ sketches/banner/sketch.js
@@ -1,8 +1,38 @@
 function setup() {
   createCanvas(1500, 500);
+  smooth();
+  noLoop();
+}
+
+function bg() {
+  background(0);
+  stroke('#fff');
+
+  for (var x = 0; x < width; x += width / 45) {
+    for (var y = 0; y < height; y += height / 15) {
+      strokeWeight(0.01);
+      line(x, 0, x, height);
+      strokeWeight(0.005);
+      line(0, y, width, y);
+    }
+  }
+}
+
+function grain() {
+  const grainAmount = 15;
+  
+  loadPixels();
+  for (let i = 0; i < pixels.length; i += 4) {
+    const grain = random(-grainAmount, grainAmount);
+    pixels[i] = pixels[i] + grain;
+    pixels[i + 1] = pixels[i + 1] + grain;
+    pixels[i + 2] = pixels[i + 2] + grain;
+  }
+  updatePixels();
 }
 
 function draw() {
-  background("#0D1117");
+  bg();
+  grain();
   // save("banner.png");
 }
